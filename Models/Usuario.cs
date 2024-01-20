1e388f3c623fc34018611ac26b524bba361114d1
isisgoncalves <isisvow@gmail.com>
2024-01-20 07:25:58 -0300

    Initial commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_Usuario.Models
+{
+    public class Usuario
+    {
+        
+    }
+}
